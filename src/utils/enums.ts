02279e2c528fc8a8fd544a1f3937472e517fbfb1
AlexDem <37954103+Alexandros-Demirtzoglou@users.noreply.github.com>
2024-06-26 22:53:10 +0300

    Add files via upload

---
+++ src/utils/enums.ts
@@ -0,0 +1,18 @@
+export enum FlightType {
+  Return = "return",
+  OneWay = "one-way",
+  MultiCity = "multi-city",
+}
+
+export enum FlightStops {
+  NonStop = "Nonstop flights",
+  MaxOneStop = "Maximum one stop",
+  All = "All",
+}
+
+export enum ClassType {
+  Economy = "Economy",
+  Premium = "Premium",
+  Business = "Business",
+  First = "First",
+}
